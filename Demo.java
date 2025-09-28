3e83dd29a31c5fa116c46bd36f891012f89c761d
Mohammad Sofyan Abdullah <sofyanrajpoot567@gmail.com>
2025-09-28 19:42:21 +0500

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,4 +1,4 @@
-public class HelloWorld {
+public class Demo {
     public static void main(String[] args) {
         System.out.println("Hello, Jenkins + EC2 + WSL!");
         System.out.println("First Edit!");
